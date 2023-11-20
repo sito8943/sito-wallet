1fb60a724fa16befc4540ffaa09a95f94004749b
Sito8943 <sito8943@gmail.com>
2023-11-20 04:51:38 -0800

    Settings sections

---
+++ src/views/Settings/sections/Accounts.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Accounts() {
+  return (
+    <section>
+      <h3 className="text-xl">Mis cuentas</h3>
+    </section>
+  );
+}
+
+export default Accounts;
---
+++ src/views/Settings/sections/BalanceTypes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BalanceTypes() {
+  return (
+    <section>
+      <h3 className="text-xl">Mis tipos de balances</h3>
+    </section>
+  );
+}
+
+export default BalanceTypes;
