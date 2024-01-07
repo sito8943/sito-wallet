b52a416fdc18d6d2620cf314bd10b69639a4f96f
Sito8943 <sito8943@gmail.com>
2024-01-07 16:51:16 -0500

    new version of @sito/ui

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fhpb06n1d2o"
+    "revision": "0.col73iqhhng"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
