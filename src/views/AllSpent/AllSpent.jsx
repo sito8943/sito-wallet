2142bd2e4643c67472c418ffd303ebec0fbdb05e
Sito8943 <sito8943@gmail.com>
2024-01-16 09:56:41 -0500

    fixed label for bars

--- src/views/AllSpent/AllSpent.jsx
+++ src/views/AllSpent/AllSpent.jsx
@@ -1,16 +1,20 @@
 import { useMemo, useCallback, useEffect, useState, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import { v4 } from "uuid";
 
 // @emotion/css
 import { css } from "@emotion/css";
 
 // @sito/ui
-import { useNotification, Button, useStyle } from "@sito/ui";
+import { useNotification, Button, useStyle, SelectControl } from "@sito/ui";
 
 // font awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+
+// providers
+import { useUser } from "../../providers/UserProvider";
 
 // utils
 import {
@@ -23,6 +27,7 @@ import {
 import Syncing from "../../components/Syncing/Syncing";
 
 // services
+import { fetchBalances, addBalance } from "../../services/wallet";
 import { fetchYears, fetchSpentByMonthNdYear } from "../../services/utils";
 
 // styles
@@ -31,6 +36,14 @@ import "./styles.css";
 export default function AllSpent() {
   const { t } = useTranslation();
   const { colors } = useStyle();
+
+  const { userState, setUserState } = useUser();
+  const [balanceType, setBalanceType] = useState(0);
+  const extraBalances = ["bills", "incomings"];
+
+  const onChangeBalanceType = (e) => {
+    console.log(e);
+  };
 
   const gradientBar = css({
     backgroundImage: `linear-gradient(0deg, #0093E900 45%, ${colors.secondary.light}78 100%)`,
@@ -72,6 +85,9 @@ export default function AllSpent() {
   }, [setNotification, t]);
 
   const localFetchSpentByMonthNdYear = useCallback(async () => {
+    // if 1 or 0 bills or incoming simple query
+    // if any balance type send balanceType id
+    // prepare rpc with params
     const { data, error } = await fetchSpentByMonthNdYear();
     if (error !== null && error.message)
       setNotification({
@@ -80,22 +96,61 @@ export default function AllSpent() {
       });
     setColumns(groupByYearAndMonth(data));
     setMax(getMaxTotalSpent(data));
-  }, [setNotification, t]);
+  }, [setNotification, t, balanceType]);
+
+  const localFetchBalanceTypes = async () => {
+    if (!userState.balances) {
+      const remoteBalances = await fetchBalances();
+      if (remoteBalances.error && remoteBalances.error !== null)
+        return console.error(remoteBalances.error.message);
+
+      if (
+        !remoteBalances.data.length &&
+        localStorage.getItem("basic-balance") === null
+      ) {
+        localStorage.setItem(
+          "basic-balance",
+          t("_accessibility:defaultValues.balanceTypes")
+        );
+        const newBalance = {
+          id: v4(),
+          description: t("_accessibility:defaultValues.balanceTypes"),
+          bill: true,
+          created_at: new Date().getTime(),
+          account: userState.account?.id,
+        };
+        if (!userState.cached) {
+          const { error } = await addBalance(newBalance);
+          if (error && error !== null) console.error(error.message);
+        }
+        setUserState({
+          type: "init-balances",
+          balances: [...(userState.balances ?? []), newBalance],
+        });
+      } else {
+        // setting
+        setUserState({
+          type: "init-balances",
+          balances: remoteBalances.data,
+        });
+      }
+    }
+  };
 
   useEffect(() => {
     localFetchYears();
     localFetchSpentByMonthNdYear();
+    localFetchBalanceTypes();
   }, []);
 
   useEffect(() => {
     if (years.length && !sync)
       setTimeout(() => {
-        console.log("hola");
         logsRef?.current?.scroll({
           top: 0,
           left:
             document.getElementById(`${currentYear}-${currentMonth}`)
-              ?.offsetLeft - 15,
+              ?.offsetLeft - 100,
           behavior: "smooth",
         });
       }, 500);
@@ -110,7 +165,7 @@ export default function AllSpent() {
 
   return (
     <main>
-      <div className="p-10 sm:p-3 pt-20 mt-20 flex flex-col gap-10 flex-1">
+      <div className="p-10 sm:p-3 pt-20 mt-20 flex flex-col gap-10 justify-start items-start flex-1">
         <div
           className={`w-10 h-10 fixed bottom-1 left-1 transition-all duration-300 ease-in-out ${
             sync ? "scale-100" : "scale-0"
@@ -119,7 +174,8 @@ export default function AllSpent() {
           <Syncing />
         </div>
       </div>
-      <div className="flex items-center">
+
+      <div className="flex items-center ml-1 gap-1">
         <Link
           to="/"
           name="to-home"
@@ -130,10 +186,32 @@ export default function AllSpent() {
         >
           <FontAwesomeIcon icon={faChevronLeft} />
         </Link>
-        <h2 className="text-6xl md:text-5xl sm:text-4xl xs:text-3xl">
-          {t("_pages:allSpent.title")}
-        </h2>
+        <div className="flex flex-col items-start justify-start">
+          <h2 className="text-6xl md:text-5xl sm:text-4xl xs:text-3xl">
+            {t("_pages:allSpent.title")}
+          </h2>
+          <SelectControl
+            value={balanceType}
+            onChange={(e) => onChangeBalanceType(e.target.value)}
+            className={`text-sm no-bg !pl-0`}
+          >
+            {extraBalances.map((extra) => (
+              <option
+                key={t(`_pages:allSpent.extraBalances.${extra}.id`)}
+                value={t(`_pages:allSpent.extraBalances.${extra}.id`)}
+              >
+                {t(`_pages:allSpent.extraBalances.${extra}.description`)}
+              </option>
+            ))}
+            {userState.balances?.map((balance) => (
+              <option key={balance.id} value={balance.id}>
+                {balance.description}
+              </option>
+            ))}
+          </SelectControl>
+        </div>
       </div>
+
       <div ref={logsRef} className="logs flex overflow-auto py-3 px-10 sm:px-3">
         {years.map((year) => (
           <div key={year} className="year-column flex flex-col gap-2 appear">
@@ -156,29 +234,35 @@ export default function AllSpent() {
                   >
                     {t(`_accessibility:monthsReduced.${month}`)}
                   </Button>
-                  <div
-                    className={`animation relative all-spent-bar flex flex-col items-start justify-between ${css(
-                      {
-                        height: `${
-                          columns[year] && columns[year][month]
-                            ? fetchPercentOf(columns[year][month], max)
-                            : 0
-                        }px`,
-                      }
-                    )} ${gradientBar}`}
-                  >
-                    {currentMonth === month && currentYear === year ? (
-                      <div
-                        className={`${dashedBar} absolute top-0 left-0 w-full h-[calc(100%-36px)]`}
-                      ></div>
-                    ) : null}
-                    <div className="top-of-the-bar secondary filled" />
-                    <div className="w-full flex flex-col items-center justify-center gap-2">
+                  <div>
+                    <div
+                      className={`animation ${
+                        columns[year] && columns[year][month] ? "mb-[2px]" : ""
+                      } relative all-spent-bar flex flex-col items-start justify-between ${css(
+                        {
+                          height: `${
+                            columns[year] && columns[year][month]
+                              ? fetchPercentOf(columns[year][month], max)
+                              : 4
+                          }px`,
+                        }
+                      )} ${gradientBar}`}
+                    >
+                      {currentMonth === month && currentYear === year ? (
+                        <div
+                          className={`${dashedBar} absolute top-0 left-0 w-full h-full`}
+                        ></div>
+                      ) : null}
+                      <div className="top-of-the-bar secondary filled" />
                       {currentMonth === month && currentYear === year ? (
                         <div className="bottom-of-the-bar secondary filled" />
                       ) : null}
-                      <p className="text-center">{columns[year][month]}</p>
                     </div>
+                    <p className="text-center h-[24px]">
+                      {columns[year] && columns[year][month]
+                        ? columns[year][month]
+                        : ""}
+                    </p>
                   </div>
                 </div>
               ))}
