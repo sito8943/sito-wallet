aab7e2768aa22521865106f1f1493bdbc303a113
SitoNumbis <sito8943@gmail.com>
2024-07-03 19:28:33 -0400

    removed unused imports

--- src/views/Auth/SignIn.jsx
+++ src/views/Auth/SignIn.jsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 // @sito/ui
