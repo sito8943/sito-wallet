cd2ea97adfddc80b80eca9c5df32e9d25cfdbf49
Sito <sito8943@gmail.com>
2025-07-29 14:55:36 -0400

    bettery padding on chip and added currency chip

--- src/views/Accounts/components/AccountCard.tsx
+++ src/views/Accounts/components/AccountCard.tsx
@@ -11,7 +11,8 @@ import { AccountType } from "lib";
 export function AccountCard(props: AccountCardPropsType) {
   const { t } = useTranslation();
 
-  const { id, onClick, actions, name, description, type, deleted } = props;
+  const { id, onClick, actions, name, description, type, currency, deleted } =
+    props;
 
   return (
     <ItemCard
@@ -29,7 +30,10 @@ export function AccountCard(props: AccountCardPropsType) {
       >
         {description ? description : t("_entities:account.description.empty")}
       </p>
-      <Chip label={String(AccountType[type])} />
+      <div className="flex gap-2">
+        <Chip label={String(AccountType[type])} />
+        <Chip label={currency?.name} />
+      </div>
     </ItemCard>
   );
 }
