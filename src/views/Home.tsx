ee83767bc312673a6b257609619e5e9145699726
Sito <sito8943@gmail.com>
2025-07-29 15:21:53 -0400

    welcome message

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,3 +1,17 @@
+import { useTranslation } from "react-i18next";
+
+// providers
+import { useAuth } from "providers";
+
 export function Home() {
-  return <main></main>;
+  const { t } = useTranslation();
+  const { account } = useAuth();
+
+  return (
+    <main className="flex">
+      <h1 className="self-center justify-self-center">
+        {t("_pages:home.welcome", { user: account?.username })}
+      </h1>
+    </main>
+  );
 }
