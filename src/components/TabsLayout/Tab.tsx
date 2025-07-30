51a04b0ecbee615e400e38df49269078510da380
Sito <sito8943@gmail.com>
2025-07-30 09:59:04 -0400

    no styles for now for tabs

--- src/components/TabsLayout/Tab.tsx
+++ src/components/TabsLayout/Tab.tsx
@@ -1,14 +1,16 @@
 // types
 import { TabPropsType } from "./types";
 
-export const  Tab = (props: TabPropsType) => {
+export const Tab = (props: TabPropsType) => {
   const { id, active, onClick, children } = props;
 
   return (
     <a
       href={`#${id}`}
       onClick={() => onClick()}
-      className={`whitespace-nowrap button !px-3 !pt-2 !border-none ${active ? "primary" : "outlined"}`}
+      className={`whitespace-nowrap button submit !px-3 !pt-2 !border-none !rounded-none ${
+        active ? "primary" : "outlined"
+      }`}
     >
       {children}
     </a>
