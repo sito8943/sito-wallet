e0655d3c00b3d39873932ae0e1921bcb1b5da980
Sito <sito8943@gmail.com>
2025-07-30 11:27:22 -0400

    fixed item card format and template

--- src/components/Card/ItemCard.tsx
+++ src/components/Card/ItemCard.tsx
@@ -1,10 +1,15 @@
 // components
 import { Actions } from "..";
 
+// lib
+import { BaseEntityDto } from "lib";
+
 // types
 import { ItemCardPropsType } from "./types.ts";
 
-export function ItemCard(props: ItemCardPropsType) {
+export function ItemCard<TRow extends BaseEntityDto>(
+  props: ItemCardPropsType<TRow>
+) {
   const {
     children,
     containerClassName = "",
@@ -17,15 +22,23 @@ export function ItemCard(props: ItemCardPropsType) {
 
   return (
     <div
-      className={`${containerClassName} flex flex-col justify-between items-start h-40 w-60 max-xs:w-full rounded-2xl p-3 group ${deleted ? "border-secondary/30" : "border-primary/30 hover:border-primary"} border-2 animated`}
+      className={`${containerClassName} flex flex-col justify-between items-start h-40 w-60 max-xs:w-full rounded-2xl p-3 group ${
+        deleted
+          ? "border-secondary/30"
+          : "border-primary/30 hover:border-primary"
+      } border-2 animated`}
     >
       <button
-        className={`${deleted ? "" : "cursor-pointer"} h-full w-full flex flex-col justify-start items-start ${className}`}
+        className={`${
+          deleted ? "" : "cursor-pointer"
+        } h-full w-full flex flex-col justify-start items-start ${className}`}
         {...rest}
       >
         {typeof title === "string" || typeof title === "number" ? (
           <h3
-            className={`text-lg ${deleted ? "!text-secondary" : "!text-text"} text-start`}
+            className={`text-lg ${
+              deleted ? "!text-secondary" : "!text-text"
+            } text-start`}
           >
             {title}
           </h3>
