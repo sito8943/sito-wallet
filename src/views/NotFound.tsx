b5436f84709bd7178297f1b86f592251e6cfbca3
Sito <sito8943@gmail.com>
2025-08-11 14:38:06 -0400

    not found

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,3 +1,15 @@
+import { Link } from "react-router-dom";
+
+import { useTranslation } from "react-i18next";
+
 export function NotFound() {
-  return <main>NOT FOUND</main>;
+  const { t } = useTranslation();
+
+  return (
+    <main className="w-full h-full items-center justify-center gap-10">
+      <h1 className="appear text-4xl !text-bg-error">{t("_pages:notFound.title")}</h1>
+      <p className="appear !text-lg text-center">{t("_pages:notFound.body")}</p>
+      <Link to="/" className="appear button primary submit !px-10">{t("_pages:home.title")}</Link>
+    </main>
+  );
 }
