fa6c1c90ffd6bc2615355d1ddeff5f2fd5ea1a5a
Sito <sito8943@gmail.com>
2025-08-15 14:02:47 +0200

    h2 on not found

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -7,7 +7,7 @@ export function NotFound() {
 
   return (
     <main className="w-full h-full items-center justify-center gap-10">
-      <h1 className="appear text-4xl !text-bg-error">{t("_pages:notFound.title")}</h1>
+      <h2 className="appear text-4xl !text-bg-error">{t("_pages:notFound.title")}</h2>
       <p className="appear !text-lg text-center">{t("_pages:notFound.body")}</p>
       <Link to="/" className="appear button primary submit !px-10">{t("_pages:home.title")}</Link>
     </main>
