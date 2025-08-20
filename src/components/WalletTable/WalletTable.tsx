f9f78bde90bc6eb327085b8c9832864ebca21cd7
Sito <sito8943@gmail.com>
2025-08-20 17:13:38 +0200

    setting total on WalletTable

--- src/components/WalletTable/WalletTable.tsx
+++ src/components/WalletTable/WalletTable.tsx
@@ -1,13 +1,20 @@
-import { Table } from "@sito/dashboard";
+import { Table, useTableOptions } from "@sito/dashboard";
 
 // types
 import { WalletTablePropsType } from "./types";
 
 // lib
 import { BaseEntityDto } from "lib";
+import { useEffect } from "react";
 
 export const WalletTable = <TRow extends BaseEntityDto>(
   props: WalletTablePropsType<TRow>
 ) => {
+  const { setTotal } = useTableOptions();
+
+  useEffect(() => {
+    setTotal(props.total);
+  }, [props.total, setTotal]);
+
   return <Table {...props} />;
 };
