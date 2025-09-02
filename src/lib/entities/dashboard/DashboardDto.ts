297e8bab29d720770e2a8d210c30c6f7b2302f17
Sito <sito8943@gmail.com>
2025-09-02 08:09:40 +0200

    dashboard dto with tilte

--- src/lib/entities/dashboard/DashboardDto.ts
+++ src/lib/entities/dashboard/DashboardDto.ts
@@ -3,6 +3,7 @@ import { DashboardCardType } from "./DashboardCardType";
 
 export interface DashboardDto extends BaseEntityDto {
   type: DashboardCardType;
+  title: string | null;
   config: string;
   position: number;
   user: CommonUserDto | null;
--- src/lib/entities/dashboard/UpdateDashboardDto.ts
+++ src/lib/entities/dashboard/UpdateDashboardDto.ts
@@ -1,6 +1,7 @@
 import { DeleteDto } from "../base";
 
 export interface UpdateDashboardDto extends DeleteDto {
+  title: string;
   config: string;
   position: number;
   userId: number;
