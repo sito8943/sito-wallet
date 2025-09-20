2b6c3a82379ec7542129128596b975d34df69229
Sito <sito8943@gmail.com>
2025-09-20 20:06:55 +0200

    fixed styles and icon button

--- src/components/Buttons/IconButton.tsx
+++ src/components/Buttons/IconButton.tsx
@@ -3,15 +3,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // types
 import { IconButtonPropsType } from "./types";
 
-const IconButton = (props: IconButtonPropsType) => {
-  const { className = "", children, icon, ...rest } = props;
+// styles
+import "./styles.css";
+
+export const IconButton = (props: IconButtonPropsType) => {
+  const { className = "", children, icon, type = "button", ...rest } = props;
 
   return (
-    <button className={`icon-button ${className}`} {...rest}>
-      <FontAwesomeIcon icon={icon} />
+    <button className={`icon-button ${className}`} {...rest} type={type}>
+      <FontAwesomeIcon icon={icon} className="!w-auto" />
       {children}
     </button>
   );
 };
-
-export default IconButton;
