f1147120e0448598cb4a709f11cc8239186e1e77
Sito <40501794+sito8943@users.noreply.github.com>
2026-01-27 10:02:46 +0100

    accordion

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
+
+// icons
+import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
 // types
 import { AccordionPropsType } from "./types";
+
+// components
+import { IconButton } from "@sito/dashboard-app";
 
 // styles
 import "./styles.css";
 
 export const Accordion = (props: AccordionPropsType) => {
-  const { open, children, className = "", contentClassName = "" } = props;
+  const {
+    open,
+    defaultOpen = false,
+    children,
+    className = "",
+    contentClassName = "",
+  } = props;
+
+  const [localOpen, setLocalOpen] = useState(open ?? defaultOpen);
 
   return (
-    <div className={`accordion-main ${className} ${open ? "open" : "closed"}`}>
+    <div
+      className={`accordion-main ${className} ${localOpen ? "open" : "closed"}`}
+    >
+      <IconButton
+        className="absolute top-4 right-2"
+        icon={localOpen ? faChevronUp : faChevronDown}
+        onClick={() => setLocalOpen(!localOpen)}
+      />
       <div className={`accordion-content ${contentClassName}`}>{children}</div>
     </div>
   );
--- src/views/Transactions/components/WeeklyCard.tsx
+++ src/views/Transactions/components/WeeklyCard.tsx
@@ -7,6 +7,7 @@ import { TransactionType } from "lib";
 import { useWeekly } from "hooks";
 
 // components
+import { Accordion } from "components";
 import { Currency } from "../../Currencies";
 import { BaseCard } from "../../Home/components/Cards/BaseCard";
 
@@ -25,6 +26,7 @@ export const WeeklyCard = (props: WeeklyCardProps) => {
     if (data) return Math.floor(data?.currentWeek - data?.previousWeek);
     return 0;
   }, [data]);
+
   const symbol = currencySymbol ?? data?.account?.currency?.symbol ?? "";
   const name = currencyName ?? data?.account?.currency?.name ?? "";
 
@@ -32,42 +34,47 @@ export const WeeklyCard = (props: WeeklyCardProps) => {
     <BaseCard className="!h-auto">
       <div className="flex flex-col gap-2 w-full">
         <h3 className="text-xl poppins">{title}</h3>
-        <div className="flex flex-wrap gap-1 items-end">
-          <p
-            className={`text-4xl font-bold self-start poppins ${
-              type === TransactionType.Out ? "text-bg-error" : "text-bg-success"
-            }`}
-          >
-            {isLoading ? "…" : data?.currentWeek}{" "}
-            <Currency name={name} symbol={symbol} />
-          </p>
-          {!!difference && !!data?.previousWeek && (
+        <Accordion defaultOpen>
+          <div className="flex flex-wrap gap-1 items-end">
             <p
-              className={`text-sm font-bold self-start poppins ${
-                difference > 0
-                  ? `${
-                      type === TransactionType.Out
-                        ? "text-bg-error"
-                        : "text-bg-success"
-                    }`
-                  : `${
-                      type === TransactionType.Out
-                        ? "text-bg-success"
-                        : "text-bg-error"
-                    }`
+              className={`text-4xl font-bold self-start poppins ${
+                type === TransactionType.Out
+                  ? "text-bg-error"
+                  : "text-bg-success"
               }`}
             >
-              <span
-                data-tooltip-id="tooltip"
-                data-tooltip-content={String(data?.previousWeek)}
-              >
-                {difference > 0 ? "+" : ""}
-                {isLoading ? "…" : difference}{" "}
-              </span>
-              <span>Last week</span>
+              {isLoading ? "…" : data?.currentWeek ?? 0}{" "}
+              <Currency name={name} symbol={symbol} />
             </p>
-          )}
-        </div>
+
+            {!!difference && !!data?.previousWeek && (
+              <p
+                className={`text-sm font-bold self-start poppins ${
+                  difference > 0
+                    ? `${
+                        type === TransactionType.Out
+                          ? "text-bg-error"
+                          : "text-bg-success"
+                      }`
+                    : `${
+                        type === TransactionType.Out
+                          ? "text-bg-success"
+                          : "text-bg-error"
+                      }`
+                }`}
+              >
+                <span
+                  data-tooltip-id="tooltip"
+                  data-tooltip-content={String(data?.previousWeek)}
+                >
+                  {difference > 0 ? "+" : ""}
+                  {isLoading ? "…" : difference}{" "}
+                </span>
+                <span>Last week</span>
+              </p>
+            )}
+          </div>
+        </Accordion>
       </div>
     </BaseCard>
   );
